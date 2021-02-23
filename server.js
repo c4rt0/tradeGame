f3a5d5ff258f9337b3848d5aad3859ead3c78e2b
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2021-02-23 14:20:32 +0000

    Creating database and table

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ con.connect(function(err){
 	// 	if (err) throw err;
 	// 	console.log("DB Created ! :)");
 	// });
+	
 	// Create TABLE
 	var sql = "CREATE TABLE testUsers(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))";
 	con.query(sql, function(err, result) {
