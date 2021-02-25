26a48127a62cc49b7f94b15c2e68574a3d627c98
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2021-02-25 18:00:19 +0000

    Connected to IEX Cloud API pulling hardcoded FB data

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+
+// for the use with API
+const request = require('request');
+
 const PORT = process.env.PORT || 5000; 	// The double pipe here allows to use the OR function, where once we deploy it
 										// locally we use port 5000, when released it uses server specific PORT... 
 const someOtherStuff = "This is some other testing stuff..."
+
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=##################_YOUR_IEX_CLOUD_TOKEN_##############', { json: true }, (err, res, body) => {
+    if (err) {return console.log(err);}
+    console.log("No error");
+    if (res.statusCode === 200) {
+        console.log(body);
+    };
+});
 
 // Ser Handlebars middleware
 app.engine('handlebars', exphbs());
