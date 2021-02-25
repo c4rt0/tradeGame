02f66bc1d23791fc83b9d85e39eed6a97954cc68
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2021-02-25 16:02:23 +0000

    Activated handlebars

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const exphbs  = require('express-handlebars');
 const path = require('path');
 const PORT = process.env.PORT || 5000; 	// The double pipe here allows to use the OR function, where once we deploy it
 										// locally we use port 5000, when released it uses server specific PORT... 
-                                  
+
+// Ser Handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set Handlebar route
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
