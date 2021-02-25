daed5e0ed49656f28ec44f35e8004c913d62561e
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2021-02-25 17:19:08 +0000

    Changed about.html to handlebars

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 While running this app remember to install node and some sort of mysql server.
 I simply installed Wampserver64 just because it's convinient, quick and easy to use. Once server is up and running, in terminal navigate to directory with server.js file and type in :
 
+To test MySQL functionality, uncomment selected part of code and run:
+
 ```bash
-node server.js
+node mysql_server.js
 ```
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs  = require('express-handlebars');
 const path = require('path');
 const PORT = process.env.PORT || 5000; 	// The double pipe here allows to use the OR function, where once we deploy it
 										// locally we use port 5000, when released it uses server specific PORT... 
+const someOtherStuff = "This is some other testing stuff..."
 
 // Ser Handlebars middleware
 app.engine('handlebars', exphbs());
@@ -11,7 +12,14 @@ app.set('view engine', 'handlebars');
 
 // Set Handlebar route
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('home', {
+        stuff: someOtherStuff
+    });
+});
+
+//create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folder
