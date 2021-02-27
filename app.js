5bd48ff3295b5113623177c5408fba4933f2c416
Adam Piasecki <c4rt0gr4ph3r@gmail.com>
2021-02-27 10:08:42 +0000

    App pulling search bar data from the external API

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
-
-// for the use with API
 const request = require('request');
-
+const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000; 	// The double pipe here allows to use the OR function, where once we deploy it
 										// locally we use port 5000, when released it uses server specific PORT... 
-const someOtherStuff = "This is some other testing stuff..."
+
+// use bodyParser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Create CALL API function
-function callApi(finishedApiCall) {
-    request('https://cloud.iexapis.com/stable/stock/fb/quote?token=_YOUR_IEX_CLOUD_TOKEN_', { json: true }, (err, res, body) => {
+function callInitialApi(finishedApiCall, ticker) {
+    var ticker = "TSLA"
+    request('https://cloud.iexapis.com/stable/stock/'+ ticker +'/quote?token=TOKEN', { json: true }, (err, res, body) => {
         if (err) {return console.log(err);}
         console.log("<<< No error detected, while connecting to IEX Cloud API >>> " + body);
         if (res.statusCode === 200) {
-            // console.log(body);
+            finishedApiCall(body);
+        };
+    });
+};
+
+// Create CALL API function
+function callApi(finishedApiCall, ticker) {
+    request('https://cloud.iexapis.com/stable/stock/'+ ticker + '/quote?token=TOKEN', { json: true }, (err, res, body) => {
+        if (err) {return console.log(err);}
+        console.log("<<< No error detected, while connecting to IEX Cloud API >>> " + body);
+        if (res.statusCode === 200) {
             finishedApiCall(body);
         };
     });
@@ -26,20 +38,37 @@ function callApi(finishedApiCall) {
 // Ser Handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
- 
 
-// Set Handlebar route
+// Set Handlebar GET route
 app.get('/', function (req, res) {
-    callApi(function(whileApiCallIsComplete) {
+    callInitialApi(function(whileApiCallIsComplete) {
         res.render('home', {
-            stock: whileApiCallIsComplete
+            stock: whileApiCallIsComplete,
         });
     });
+});
+
+// Set Handlebar POST TICKER route
+app.post('/ticker', function (req, res) {
+    callApi(function(whileApiCallIsComplete) {
+        res.render('ticker', {
+            stock: whileApiCallIsComplete,
+        });
+    }, req.body.stockTicker);
 });
 
 //create about page route
 app.get('/about.html', function (req, res) {
     res.render('about');
+});
+
+// Set Handlebar GET TICKER route
+app.get('/ticker', function (req, res) {
+    callApi(function(whileApiCallIsComplete) {
+        res.render('ticker', {
+            stock: whileApiCallIsComplete,
+        });
+    });
 });
 
 // Set static folder
